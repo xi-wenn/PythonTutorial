036c2d5bfc747196d9ff43c107306e1cd2473fb1
Shifan Zhang <shz144@ucsd.edu>
2018-09-30 23:24:43 -0400

    part5 debugged

--- project1.py
+++ project1.py
@@ -130,17 +130,18 @@ del movie_rating_matrix
 
 #the smallest prime number larger than 4499 (total number of movies)
 R = 4507
+a_list = np.random.choice(R, SIGNATURE_MATRIX_ROWS)
+b_list = np.random.choice(R, SIGNATURE_MATRIX_ROWS)
 signature_matrix = np.zeros((SIGNATURE_MATRIX_ROWS, USER_COUNT))
 for i in range(SIGNATURE_MATRIX_ROWS):
   # first hash original 20 row matrix
-  a = random.randint(0, R - 1)
-  b = random.randint(0, R - 1)
+  a = a_list[i]
+  b = b_list[i]
   signature_matrix[i] = np.amin(np.remainder((compressed_movie_rating_matrix * a + b), R), axis = 0)
 
-# pdb.set_trace()
 
 r = 11
-b = 91
+band_num = 91
 P = 45491
 
 # def remove_single_appearance_values(vals, count):
@@ -157,26 +158,25 @@ P = 45491
 
 
 # close_user_pairs = set()
-false_positives = set()
+# false_positives = set()
 actual_close_pairs = set()
-for band_index in range(b):
-  a = np.diag(np.random.choice(P, r))
-  b = np.random.choice(P, r).reshape((r, 1))
+a_diag = np.diag(np.random.choice(P, r))
+b_col = np.random.choice(P, r).reshape((r, 1))
+list_buckets = []
+for band_index in range(band_num):
   cur_band_matrix = signature_matrix[band_index * r : (band_index + 1) * r, :]
-
-  res_mat = (a @ cur_band_matrix + b) % P
+  res_mat = (a_diag @ cur_band_matrix + b_col) % P
   val_list = np.sum(res_mat, axis = 0)
   # vals, count = np.unique(val_list, return_counts=True)
   # repeated_val = remove_single_appearance_values(vals, count)
-
 
   buckets = {}
   # map(lambda i, x, buckets = buckets: buckets[x] = buckets.get(x, []) + [i], enumerate(val_list))
   for idx, value in enumerate(val_list):
     # if value in repeated_val:
       # pdb.set_trace()
-    buckets[(band_index, value)] = buckets.get((band_index, value), []) + [idx]
-
+    buckets[value] = buckets.get((value), []) + [idx]
+  list_buckets.append(buckets)
   # pdb.set_trace()
   for bucket_key, bucket_values in buckets.items():
     if len(bucket_values) > 1:
@@ -185,12 +185,12 @@ for band_index in range(b):
         pair_set = frozenset(pair)
 
         # if already seen pair before, continue
-        if (pair_set in false_positives) or (pair_set in actual_close_pairs):
+        if (pair_set in actual_close_pairs):
           continue
 
         # else calculate jaccard distance and add to appropriate set
         user_1_data, user_2_data = compressed_movie_rating_matrix[:,user_1], compressed_movie_rating_matrix[:,user_2]
-        intersection = np.count_nonzero(user_1_data[user_1_data > 0] == user_2_data[user_1_data > 0])
+        intersection = len(np.intersect1d(user_1_data[user_1_data > 0], user_2_data[user_2_data > 0]))
         union = np.count_nonzero(np.unique(np.append(user_1_data, user_2_data)))
 
         if union == 0:
@@ -199,8 +199,8 @@ for band_index in range(b):
           print(union)
         if (1 - (intersection / union)) < 0.35:
           actual_close_pairs.add(pair_set)
-        else:
-          false_positives.add(pair_set)
+        # else:
+        #   false_positives.add(pair_set)
         # col_idx_1, col_idx_2 = pair
         # temp_list = []
         # temp_list.append(column_idx_user_id_map[col_idx_1])
@@ -209,7 +209,7 @@ for band_index in range(b):
         # close_user_pairs.add(frozenset(pair))
 
   print(band_index)
-  print(len(false_positives))
+  # print(len(false_positives))
   print(len(actual_close_pairs))
   # print(close_user_pairs)
 
@@ -224,6 +224,85 @@ for band_index in range(b):
 #     actual_similar_pairs.add(pair)
 
 # print(len(actual_similar_pairs))
+
+###############    Part 5       #######################################################
+def find_nearest_user(new_movie_list, list_buckets):
+
+  #map the movie_id in the given list to 1 - 4499 by above dictionary
+  compressed_movie_rating_vec = np.zeros(20, dtype='int16')
+  row_count = 0
+  for movie_id in new_movie_list:
+    compressed_movie_rating_vec[row_count] = movie_id_row_idx_map[movie_id] + 1
+    row_count += 1
+
+  pdb.set_trace()
+
+  #get the 1001 by 1 signature vector
+  signature_vec = np.zeros(SIGNATURE_MATRIX_ROWS)
+  for i in range(SIGNATURE_MATRIX_ROWS):
+    a = a_list[i]
+    b = b_list[i]
+    signature_vec[i] = np.amin(np.remainder((compressed_movie_rating_vec * a + b), R))
+
+  pdb.set_trace()
+
+  #hash the signature vector to get the bucket values
+  bucket_dict_new = {}
+  for band_idx in range(band_num):
+    cur_band_vec = signature_vec[band_idx * r : (band_idx + 1) * r]
+    res_mat = (a_diag @ cur_band_vec.reshape((r,1)) + b_col) % P
+    val = np.sum(res_mat, axis = 0)
+    bucket_dict_new[band_idx] = val
+
+  pdb.set_trace()
+  # min_distance = 1
+  # nearest_user_id = -1
+  nearest_set = set()
+  #loop all the bucket values, find nearest neighbor
+  for band_idx, val in bucket_dict_new.items():
+    #get the related set of similar movies
+    user_list = (list_buckets[band_idx]).get(int(val), [])
+    if len(user_list) == 0: continue
+    for user_id in user_list:
+      user_1_data , user_2_data = compressed_movie_rating_vec, compressed_movie_rating_matrix[:,user_id]
+      intersection = len(np.intersect1d(user_1_data[user_1_data > 0], user_2_data[user_2_data > 0]))
+      union = np.count_nonzero(np.unique(np.append(user_1_data, user_2_data)))
+      jaccard_dist = 1 - (intersection / union)
+      if jaccard_dist < 0.35:
+        nearest_set.add(column_idx_user_id_map[user_id])
+    pdb.set_trace()
+
+  return nearest_set
+
+pdb.set_trace()
+new_movie_list = [178, 761, 4432]
+nearest_user = find_nearest_user(new_movie_list, list_buckets)
+pdb.set_trace()
+print(nearest_user)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
