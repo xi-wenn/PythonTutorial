338172ad2f2b4f51c5fb6f4d9c7efe0e40a7bda9
Xi Wen <baozi.wx@gmail.com>
2018-10-28 15:50:09 -0400

    problem 1b

--- Project2/project2.py
+++ Project2/project2.py
@@ -2,14 +2,14 @@ from sklearn.datasets import fetch_rcv1
 from scipy import sparse
 import numpy as np
 
+######################################### Prep work ############################################
 CCAT_ROW_IDX = 33
-
-
+TRAIN_SIZE = 100000
 rcv1 = fetch_rcv1()
 num_rows, num_columns = rcv1.data.shape
-# print(num_rows)
-# print(num_columns)
 
+
+############################ Problem 1a: generate CCAT vector ###################################
 ccat_vector = np.full((num_rows), -1)
 
 for i in range(num_rows):
@@ -19,17 +19,14 @@ for i in range(num_rows):
 # print(ccat_vector)
 # print(np.where(ccat_vector == 1)[0].size)
 
-# print(np.where(rcv1.target_names == 'CCAT'))
 
-# print((0, 700) in get_items(rcv1['data']))
-# print((0, 2292) in get_items(rcv1['data']))
-# print((0, 2) in get_items(rcv1['target']))
-# print((0, 93) in get_items(rcv1['target']))
+########################### Problem 1b: split training test set ###################################
+data_train  = rcv1.data[:TRAIN_SIZE, :]
+data_test   = rcv1.data[TRAIN_SIZE:, :]
+label_train = ccat_vector[:TRAIN_SIZE]
+label_test = ccat_vector[TRAIN_SIZE:]
 
-# print(rcv1['data'][0][700])
-# print(rcv1['data'][0][2292])
-# print(rcv1['target'][0][2])
-# print(rcv1['target'][0][93])
-
-# print(rcv1['data'])
-# print(rcv1['target'])
+# print(data_train)
+# print(data_test)
+# print(label_train)
+# print(label_test)
