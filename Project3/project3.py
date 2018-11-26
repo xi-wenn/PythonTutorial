ff3ec72dea85b947d7cabf71bc59ab688fe7115e
Xi Wen <baozi.wx@gmail.com>
2018-11-25 20:37:18 -0500

    change p2 p3 to functions

--- Project3/project3.py
+++ Project3/project3.py
@@ -53,26 +53,18 @@ means = np.mean(data, axis = 0)
 data = data / means
 print(data)
 
-
+############################# problem 2 ###################################
 #online K-means Algorithm (mini-batch)
 import random
-
-#centroids is np.array, x is a data points
-def center(x, centroids):
-    diff = centroids - x
-    diff = diff**2
-    center_index = np.argmin(np.sum(diff, axis = 1))
-    return center_index
-
-B = 1000 #batch-size
-K = 100 #number of centorids
-T = 100 #max number of iterations
 
 #get K random centroids(randomly select K data points, not using k-means++)
 indices = random.sample(range(data.shape[0]), K)
 centroids = data[indices]
-
-for t in range(1, T+1):
+def run_kmeans(data, K, centroids):
+  B = 1000 #batch-size
+  #   K = 100 #number of centorids
+  T = 1000 #max number of iterations
+  for t in range(1, T+1):
     eta = 1/t
     #print(t)
 
@@ -82,53 +74,39 @@ for t in range(1, T+1):
 
     #calculate the center(x)
     for i in range(B):
-        x_i = batch_points[i]
-        center_index = center(x_i, centroids)
+      x_i = batch_points[i]
+      center_index = center(x_i, centroids)
 
-        #update the cur_center
-        centroids[center_index] += eta * (x_i - centroids[center_index])
+      #update the cur_center
+      centroids[center_index] += eta * (x_i - centroids[center_index])
+  return centroids
 
-
+####################### problem 3 ####################################
 #return minimum distance square of the point and the current centroids
 def min_distance(x, centroids):
     diff = centroids - x
     diff = diff**2
     return min(np.sum(diff, axis = 1))
 
-#randomly select the first centroid
-K = 100
-centroids = np.zeros([K, data.shape[1]])
-centroids[0] = (data[random.sample(range(data.shape[0]), 1)])
-centroids[0].shape
+def select_kmpp_centroids(K):
+  #randomly select the first centroid
+#   K = 100
+  centroids = np.zeros([K, data.shape[1]])
+  centroids[0] = (data[random.sample(range(data.shape[0]), 1)])
+  centroids[0].shape
 
-distance_list = [0.0]*data.shape[0]
+  distance_list = [0.0]*data.shape[0]
 
-for i in range(1, K):
-    for p_index in range(data.shape[0]):
-        distance_list[p_index] = min_distance(data[p_index], centroids[0:i, :])
+  for i in range(1, K):
+      for p_index in range(data.shape[0]):
+          distance_list[p_index] = min_distance(data[p_index], centroids[0:i, :])
 
-    total = sum(distance_list) * random.random()
-    for ind, val in enumerate(distance_list):
-            total -= val
-            if total > 0:
-                continue
-            centroids[i] = data[ind]
-            break
-
-#k-means
-for t in range(1, T+1):
-    eta = 1/t
-    #print(t)
-
-    #randomly get mini-batch size data points
-    mini_batch_indices = random.sample(range(data.shape[0]), B)
-    batch_points = data[mini_batch_indices]
-
-    #calculate the center(x)
-    for i in range(B):
-        x_i = batch_points[i]
-        center_index = center(x_i, centroids)
-
-        #update the cur_center
-        centroids[center_index] += eta * (x_i - centroids[center_index])
+      total = sum(distance_list) * random.random()
+      for ind, val in enumerate(distance_list):
+              total -= val
+              if total > 0:
+                  continue
+              centroids[i] = data[ind]
+              break
+  return centroids
 
